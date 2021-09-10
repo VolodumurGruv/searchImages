f04c05a98b0cc2e0f8698020cf844dd66078ed24
VolodumurGruv <gru.v@i.ua>
2021-09-10 22:29:49 +0300

    finished sidenav and tool nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,31 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CommonModule } from '@angular/common';
-import { SideBarComponent } from './shared/components/side-bar/side-bar.component';
 import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
 import { SharedModule } from './shared/shared.module';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
-  declarations: [AppComponent, SideBarComponent, MainLayoutComponent],
+  declarations: [AppComponent, MainLayoutComponent],
   imports: [
     BrowserModule,
     CommonModule,
+    FormsModule,
     SharedModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatSidenavModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatListModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { FlickrService } from './services/flickr.service';
 
 @NgModule({
   imports: [HttpClientModule],
-  // providers: [{ provide: FlickrService, useClass: FlickrService }],
 })
 export class SharedModule {}
